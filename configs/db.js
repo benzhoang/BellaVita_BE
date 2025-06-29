39ca0acc10930c64849f995c7a680f7488d53a82
AuraNVC <phongnguyen2012015@gmail.com>
2025-06-29 15:14:14 +0700

    change db config

--- configs/db.js
+++ configs/db.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: process.env.PGSSL === 'true' ? { rejectUnauthorized: false } : false,
-  },
-  logging: false, // bỏ log SQL nếu không cần
-});
-
-// Kiểm tra kết nối
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('✅ Đã kết nối PostgreSQL qua Sequelize thành công!');
-  } catch (err) {
-    console.error('❌ Lỗi kết nối Sequelize:', err.message);
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Bắt buộc với Supabase
+      }
+    },
+    logging: false
   }
-})();
+);
 
 module.exports = sequelize;
